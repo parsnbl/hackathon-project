2254ac09acd451d72e2149bf740db2bdd89b1473
Evan Pearson <parsnbl@icloud.com>
2023-10-12 10:48:05 -0700

    made the query selector logic

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,8 @@
-
+document.querySelectorAll('input[name="animal"]').forEach((elem)=>{
+  elem.addEventListener('change', async function(event) {
+    await changeAnimalSetting();
+  });
+});
 
 async function changeAnimalSetting() {
   const currentAnimal = await chrome.storage.sync.get("animal");

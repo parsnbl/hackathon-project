9ced5d79a41660fdbee20796a9d2cba140f49bf6
Evan Pearson <parsnbl@icloud.com>
2023-10-12 16:51:28 -0700

    fixed the messaging errors

--- scripts/background.js
+++ scripts/background.js
@@ -1,5 +1,20 @@
 let cache = {}
 
+//refresh content scripts
+chrome.runtime.onInstalled.addListener(async () => {
+  for (const contentScript of chrome.runtime.getManifest().content_scripts) {
+    for (const tab of await chrome.tabs.query({url: contentScript.matches})) {
+      if (tab.url.slice(0,9) !== 'chrome://') {
+        chrome.scripting.executeScript({
+          target: {tabId: tab.id},
+          files: contentScript.js,
+        });
+      }
+    }
+  }
+});
+
+//add failed requests to the cache
 chrome.webRequest.onCompleted.addListener(
   async (details) => {
     if (details.type === 'main_frame' && details.statusCode > 400) {
@@ -11,14 +26,21 @@ chrome.webRequest.onCompleted.addListener(
   { urls: ['<all_urls>'] }
 );
 
+//when the navigation is finished, send to the outermost frame the cache, then empty it
 chrome.webNavigation.onCompleted.addListener(async details => {
-    if (cache["details"] && details.tabId === cache["tabId"]) {
+  console.log(details)
+    if (cache["details"] && details.tabId === cache["tabId"] && details.frameType === 'outermost_frame') {
+      try {
         const response = await chrome.tabs.sendMessage(cache["tabId"] ,cache["details"]);
         cache = {}
-        console.log(response)
+      }  catch(err) {
+        console.error('err:', err, 'cache:', cache, 'details:', details);
+      }
+      
     }
 })
 
+// set the icon whenever we change it
 chrome.storage.onChanged.addListener((changes, area) => {
   if (area === 'local') {
     chrome.storage.local.get('ANIMAL', result => {
